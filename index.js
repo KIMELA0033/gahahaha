d9eda75503406dbfb023d2c092eb96e1f565d56d
KIMELA0033 <100943383+KIMELA0033@users.noreply.github.com>
2022-12-16 22:54:09 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// 変数
+let unko = 'hello world!';
+
+// unko = 'hello world2!!';
+
+// 定数
+const bigUnko = 'he...hell...hello World'
+
+// bigUnko = 'helloworld2!!'
+
+// 配列
+let inoki = ['i-ti','ni-i','sa-n','da--!!'];
+
+// ループ分
+let index = 0;
+while(index < inoki.lenth) {
+  // 繰り返したい命令
+  console.log(index);
+    index++;  //index = 5
+}
+
+
+
+console.log(inoki);
